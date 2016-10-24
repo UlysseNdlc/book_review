9cd392a2180ce7203273b8d2aa84386da2123769
UlysseNdlc <ulysse.nedelec@gmail.com>
2016-10-24 18:12:54 +0200

    Ajout du système d'étoile avec jquery.raty pour voir les notes des films

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
 before_action :set_review, only: [:show, :edit, :update, :destroy]
+before_action :set_book
 before_action :authenticate_user!
 
 	def index
@@ -21,9 +22,10 @@ before_action :authenticate_user!
 	def create
 	@review = Review.new(review_params)
 	@review.user_id = current_user.id
+	@review.book_id = @book.id
 
 	if @review.save
-		redirect_to @review
+		redirect_to @book
 	else
 	render 'new'
 	end
@@ -42,7 +44,9 @@ before_action :authenticate_user!
 	def set_review
 	@review = Review.find(params[:id])
 	end
-
+	def set_book
+	@book = Book.find(params[:book_id])
+	end
 # Never trust parameters from the scary internet, only allow the white list through.
 	def review_params
 params.require(:review).permit(:rating, :comment)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023090357) do
+ActiveRecord::Schema.define(version: 20161024135832) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20161023090357) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "book_id"
   end
 
   create_table "users", force: :cascade do |t|
